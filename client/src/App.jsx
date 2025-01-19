9d5115bd26470147397ed50bef76c3f4481e5df2
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-01-18 21:21:43 -0800

    removed react-ga as it is not supported and put analytics in index.html instead

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import WelcomePage from './components/pages/WelcomePage';
 import AboutPage from './components/pages/AboutPage';
@@ -10,35 +9,9 @@ import PrivacyPolicyPage from './components/pages/PrivacyPolicy';
 import TermsAndConditionsPage from './components/pages/Terms';
 import ScrollToTop from './components/ScrollToTop';
 
-const Tracking_ID = import.meta.env.VITE_GOOGLE_TRACKING_ID;
-ReactGA.initialize(Tracking_ID);
-
-
-function AnalyticsTracker() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const path = location.pathname + location.search;
-
-    // Log pageview
-    ReactGA.pageview(path);
-
-    // Check for how many users for the main website
-    if (path === "/") {
-      ReactGA.event({
-        category: "Page Visit",
-        action: "Home Page Viewed",
-      });
-    }
-  }, [location]);
-
-  return null;
-}
-
 function App() {
   return (
     <Router>
-      <AnalyticsTracker />
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainLayout><WelcomePage /></MainLayout>} />
